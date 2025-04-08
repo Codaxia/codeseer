c05f7e9c5472d56d2cd99a55711631ccfdd52b6b
rprethi <2271821@cmontmorency.qc.ca>
2025-04-08 17:59:28 -0400

    ajustement et corrections!

--- script.js
+++ script.js
@@ -23,4 +23,42 @@ const swiper = new Swiper(".role-swiper", {
       },
     },
   });
-  
+
+  const preInscriptionBtn01 = document.getElementById('preInscriptionBtn01');
+  const preInscriptionBtn02 = document.getElementById('preInscriptionBtn02');
+  const preInscriptionBtn03 = document.getElementById('preInscriptionBtn03');
+  const preInscriptionBtn04 = document.getElementById('preInscriptionBtn04');
+  const preInscriptionModal = document.getElementById('preInscriptionModal');
+  const closeModalBtn = document.getElementById('closeModalBtn');
+
+  // Ouvrir la modal et désactiver le scroll du body
+  preInscriptionBtn01.addEventListener('click', () => {
+    preInscriptionModal.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+  });
+
+  preInscriptionBtn02.addEventListener('click', () => {
+    preInscriptionModal.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+  });
+
+  preInscriptionBtn03.addEventListener('click', () => {
+    preInscriptionModal.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+  });
+
+  preInscriptionBtn04.addEventListener('click', () => {
+    preInscriptionModal.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+  });
+
+  // Fermer la modal et réactiver le scroll du body
+  function closeModal() {
+    preInscriptionModal.style.display = 'none';
+    document.body.style.overflow = 'none';
+  }
+
+  // Événement pour le bouton de fermeture
+  closeModalBtn.addEventListener('click', closeModal);
+
+  // Fermer la modal si l'utilisateur clique sur l'overlay
